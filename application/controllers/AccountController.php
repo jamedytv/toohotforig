df34ca1ce6ecc7391d3aaa2956fa3f51aca96e0e
Kincade71 <kincade71@gmail.com>
2013-10-17 02:56:08 -0400

    Readded deleted controllers

---
+++ application/controllers/AccountController.php
@@ -0,0 +1,39 @@
+<?php
+
+class AccountController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function createAction()
+    {
+        // action body
+    }
+
+    public function removeAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
